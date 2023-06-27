5f2d77afde6d5a06b34238afc85885ec11b97c84
Govind Jangra <govind70821@gmail.com>
2023-06-27 13:48:27 +0530

    deployment

--- cloudinary.js
+++ cloudinary.js
@@ -1,9 +1,9 @@
 import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
-  cloud_name: "sathya195",
-  api_key: "559255822375866",
-  api_secret: "HNkW35fXt0duvQm0g_TPLk4IIvs",
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
 });
 
 export default cloudinary;
